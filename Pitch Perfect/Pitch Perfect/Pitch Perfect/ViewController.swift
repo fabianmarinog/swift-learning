412b09a2497ec2aa9d2801fa96840328be6d21ca
fabian marino <alvarofabianmarinho@gmail.com>
2015-06-02 20:32:30 -0500

    Includes Pitch Perfect iOS Swift project.

---
+++ Pitch Perfect/Pitch Perfect/Pitch Perfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by fabian marino on 6/1/15.
+//  Copyright (c) 2015 Globant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.k
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
